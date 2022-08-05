759e63fafaa67ae10ad4064c223ee204a6ce227c
Nick <87833874+vcnthomas@users.noreply.github.com>
2022-08-05 17:40:54 -0600

    Established routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/horses', function(){
+    return view('horses');
+});
+
+Route::get('/stocks', function(){
+    return view('stocks');
+});
+
+Route::get('/stocks/vld', function(){
+    return view('vld');
+});
